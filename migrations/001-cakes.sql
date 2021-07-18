2b5bf1546f9cba8d221c0bc697dde085dfa1ae57
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2021-07-18 20:46:32 +0100

    updated package.json dev and build scripts

--- migrations/001-cakes.sql
+++ migrations/001-cakes.sql
@@ -20,29 +20,29 @@ CREATE TABLE Ingredientes (
   unidade TEXT
 );
 
-INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de Chocolate', 2);
-INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de cenoura', 2);
-INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de morango', 1);
+-- INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de Chocolate', 2);
+-- INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de cenoura', 2);
+-- INSERT INTO Cake_name (nome_bolo, quantidade) VALUES ('Bolo de morango', 1);
 
-INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('ovos', 'uni');
-INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('farinha', 'g');
-INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('açucar', 'g');
-INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('chocolate', 'g');
-INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('cenoura', 'g');
+-- INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('ovos', 'uni');
+-- INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('farinha', 'g');
+-- INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('açucar', 'g');
+-- INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('chocolate', 'g');
+-- INSERT INTO Ingredientes (nome_ingrediente, unidade) VALUES ('cenoura', 'g');
 
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 1, 2);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 2, 200);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 3, 150);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 4, 150);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 1, 2);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 2, 200);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 3, 150);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (1, 4, 150);
 
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 1, 2);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 2, 200);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 3, 150);
-INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 5, 150);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 1, 2);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 2, 200);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 3, 150);
+-- INSERT INTO Cake (nome_id, ingredientes_id, quantidade) VALUES (2, 5, 150);
 
 -- Down
 
-DROP TABLE Cake;
-DROP TABLE Ingredientes;
-DROP TABLE Cake_name;
+-- DROP TABLE Cake;
+-- DROP TABLE Ingredientes;
+-- DROP TABLE Cake_name;
 
